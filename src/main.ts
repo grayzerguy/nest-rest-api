c8f0a9bbf794f3cc8af062c750d020706d9be111
grayzerguy <grayzerg@gmail.com>
2023-04-25 12:37:18 +0300

    25.4

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// This is the entry point of the application
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,4 +6,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+//bootstrap is a function that returns a promise that resolves to an instance of the Nest application
 bootstrap();
